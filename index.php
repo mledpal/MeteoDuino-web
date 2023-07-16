27ffadbc638c1423169cae4968a97018cd1a703e
Miguel Ledesma Palacios <mledpal@gmail.com>
2023-07-16 19:35:26 +0200

    Mejora en interfaz. Más datos visualizados

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=icon href='img/thermostat.svg' sizes="32x32" type="image/svg">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Monitor de Temperatura</title>
+</head>
+<body>
+  <aside>
+    <form id="form">
+      <label for="modo">Gráfica</label>
+      <select name="modo" id="modo">
+        <option value="24h">24 Horas</option>
+        <option value="temperaturas">Temperaturas</option>        
+        <option value="otros">Presion / Humedad </option>        
+        <option value="last14days">14 días anteriores</option>
+        <option value="otros">Presion / Humedad </option>           
+      </select>        
+    </form> 
+    
+    <section id="datos">      
+      <div id="datos">
+        <p><span class="titulo hora">Hora</span><span class="dato hora" id="tiempo"></span></p>  
+        <p><span class="titulo">T.Actual</span><span class="dato" id="tactual"></span></p>
+        <p><span class="titulo">T.Máxima</span><span class="dato" id="tmaxima"></span></p>
+        <p><span class="titulo">T.Media</span><span class="dato" id="tmedia"></span></p>
+        <p><span class="titulo">T.Mínima</span><span class="dato"  id="tminima"></span></p>       
+        <p><span class="titulo">H.Actual</span><span class="dato" id="hactual"></span></p>
+        <p><span class="titulo">H.Máxima</span><span class="dato" id="hmaxima"></span></p>
+        <p><span class="titulo">H.Media</span><span class="dato" id="hmedia"></span></p>
+        <p><span class="titulo">H.Mínima</span><span class="dato"  id="hminima"></span></p>
+        <p><span class="titulo">P.Actual</span><span class="dato" id="pactual"></span></p>
+        <p><span class="titulo">P.Máxima</span><span class="dato" id="pmaxima"></span></p>
+        <p><span class="titulo">P.Media</span><span class="dato" id="pmedia"></span></p>
+        <p><span class="titulo">P.Mínima</span><span class="dato" id="pminima"></span></p>   
+      </div>
+
+    </section>
+
+
+  </aside>
+    
+  <div id="graph">
+      <canvas id="grafico"></canvas>
+  </div>
+
+
+  <script src="./js/chart.js"></script>
+  <script src="./js/graph.js" type="module"></script>  
+  <script src="./js/index.js" type="module"></script>
+    
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,125 @@
+"use strict";
+
+import { graph24, graphTemperaturas, graphOtros, graphLastDays } from './graph.js';
+
+const url = 'http://ledemar.ddns.net/chart/api.php';
+
+const opcion = document.getElementById('modo');
+const tiempo = document.getElementById('tiempo');
+const TMaxima = document.getElementById('tmaxima');
+const TMinima = document.getElementById('tminima');
+const TMedia = document.getElementById('tmedia');
+const TActual = document.getElementById('tactual');
+
+const HMaxima = document.getElementById('hmaxima');
+const HMinima = document.getElementById('hminima');
+const HMedia = document.getElementById('hmedia');
+const HActual = document.getElementById('hactual');
+
+const PMaxima = document.getElementById('pmaxima');
+const PMinima = document.getElementById('pminima');
+const PMedia = document.getElementById('pmedia');
+const PActual = document.getElementById('pactual');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  opcion.addEventListener('change', (e) => {
+    e.preventDefault();  
+    destroyGraph();  
+    let modo = opcion.value;
+    drawGraph(modo);  
+    
+  });
+
+});
+
+function destroyGraph() {
+  document.getElementById('grafico').remove();    
+  let canvas = document.createElement('canvas');
+  canvas.setAttribute('id', 'grafico');
+  document.getElementById('graph').appendChild(canvas);    
+}
+
+function cleanData() {
+  TMaxima.textContent = ``;
+  TMinima.textContent = ``;
+  TMedia.textContent = ``;
+  TActual.textContent = ``;
+  tiempo.textContent = '';
+  HMaxima.textContent = ``;
+  HMinima.textContent = ``;
+  HMedia.textContent = ``;
+  HActual.textContent = ``;
+  PMaxima.textContent = ``;
+  PMinima.textContent = ``;
+  PMedia.textContent = ``;
+  PActual.textContent = ``;  
+}
+
+
+async function drawGraph(modo) {
+
+  const options = {
+    method: "POST",
+    body: `modo=${modo}`,    
+    cors: 'no-cors',
+    headers: {      
+      'Access-Control-Allow-Origin': '*',
+      'cors': 'no-cors',
+      'Content-type': 'application/x-www-form-urlencoded',        
+      cache: 'no-cache'        
+    }
+  }
+
+  const response = await fetch(url, options);  
+  const datos = await response.json();
+  
+  switch(modo) {
+
+    case '24h':
+      graph24(datos[0], datos[1], datos[2], datos[3], datos[4]);     
+      TActual.textContent = `${datos[1][datos[1].length-1]} ºC`;  
+      TMaxima.textContent = `${Math.max(...datos[1])} ºC`;
+      TMinima.textContent = `${Math.min(...datos[1])} ºC`;
+      TMedia.textContent = `${Math.round(datos[1].reduce((a, b) => a + b, 0) / datos[1].length, 2)} ºC`;
+      tiempo.textContent = datos[0][datos[0].length-1];
+      
+      HActual.textContent = `${datos[3][datos[3].length-1]} %`;
+      HMaxima.textContent = `${Math.max(...datos[3])} %`;
+      HMinima.textContent = `${Math.min(...datos[3])} %`;
+      HMedia.textContent = `${Math.round(datos[3].reduce((a, b) => a + b, 0) / datos[3].length, 2)} %`;
+
+      PActual.textContent = `${datos[4][datos[4].length-1]} hPa`;
+      PMaxima.textContent = `${Math.max(...datos[4])} hPa`;
+      PMinima.textContent = `${Math.min(...datos[4])} hPa`;
+      PMedia.textContent = `${Math.round(datos[4].reduce((a, b) => a + b, 0) / datos[4].length, 2)} hPa`;      
+      break;
+
+    case 'temperaturas':  
+      cleanData();          
+      graphTemperaturas(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6]);
+      break;
+
+    case 'otros':       
+      cleanData();   
+      graphOtros(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6]);
+      break;
+
+    case 'last14days':      
+      graphLastDays(datos[0].reverse(), datos[1].reverse(), datos[2].reverse(), datos[3].reverse(), datos[4].reverse());
+      TMaxima.textContent = `${Math.max(...datos[1])} ºC`;
+      TMinima.textContent = `${Math.min(...datos[1])} ºC`;
+      TMedia.textContent = `${Math.round(datos[1].reduce((a, b) => a + b, 0) / datos[1].length, 2)} ºC`;
+
+      tiempo.textContent = 'Hora';
+
+      break;
+  }; 
+  
+};
+
+
+
+
+drawGraph('24h');  // Dibuja el gráfico por defecto al cargar la página
